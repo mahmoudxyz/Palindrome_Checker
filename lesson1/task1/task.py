eb6a1b1d08767d768c77b44242e3a9e2931e5228
Mahmoud Ahmed <mahmoudahmedxyz@gmail.com>
2023-05-04 11:35:44 +0300

    remove solution

--- lesson1/task1/task.py
+++ lesson1/task1/task.py
@@ -1,19 +1,2 @@
-def palindrome_checker(word):
-
-    # Convert to lowercase
-    word = word.lower()
-
-    # Remove all non-alphanumeric characters
-    word = ''.join(c for c in word if c.isalnum())
-
-    # Check if the word is a palindrome
-    reverse_word = word[::-1]
-    is_palindrome = word == reverse_word
-
-    # Return the appropriate message
-    if is_palindrome:
-        message = f"{word} is a palindrome!"
-    else:
-        message = f"{word} is not a palindrome."
-
-    return message
+def palindrome_checker():
+ # write code here
